0d960430cf3443719861f9525540bd41479fa4ed
Owen Limantoro <limjiwen01@gmail.com>
2026-04-19 18:51:55 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Owen Limantoro on 19/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
