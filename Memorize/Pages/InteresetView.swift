316e5c459c69c6b82c1fd8e5025b52d39bad2c71
Owen Limantoro <limjiwen01@gmail.com>
2026-04-19 20:32:26 +0800

    Added File Structure

---
+++ Memorize/Pages/InteresetView.swift
@@ -0,0 +1,21 @@
+//
+//  InteresetView.swift
+//  Memorize
+//
+//  Created by Owen Limantoro on 19/04/26.
+//
+
+import SwiftUI
+
+struct InteresetView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Hello World")
+        }
+        .navigationTitle("Interest")
+    }
+}
+
+#Preview {
+    InteresetView()
+}
