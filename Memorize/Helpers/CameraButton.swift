0dfec3f7de00044a308fc01cdc0195a7bf315743
Owen Limantoro <limjiwen01@gmail.com>
2026-04-21 16:37:58 +0800

    testing userinterfacestate

--- Memorize/Helpers/CameraButton.swift
+++ Memorize/Helpers/CameraButton.swift
@@ -15,6 +15,7 @@ struct CameraButton: View {
     var body: some View {
         
         // This button is used to take pictures
+        // hi comment
         // This is a second comment to test git
         // Hello there, try to add a change
         // Test to see if gitignore works
