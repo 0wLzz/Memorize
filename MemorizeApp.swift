a605c4cdebf1249ddb6566652fcdd40c7516416a
PhoenixWry2 <henshincool@gmail.com>
2026-07-16 08:40:48 +0800

    Unchecked all files' target memberships and put them into Legacy and recomment on MemorizeApp
    Put everything in Legacy so I can focus on rebuilding the repository for SwiftData

--- MemorizeApp.swift
+++ MemorizeApp.swift
@@ -5,16 +5,27 @@
 //  Created by Owen Limantoro on 19/04/26.
 //
 
+//import SwiftUI
+//
+//@main
+//struct MemorizeApp: App {
+//    @StateObject private var repo = PersonRepository()
+//    
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//                .environmentObject(repo)
+//        }
+//    }
+//}
+
 import SwiftUI
 
 @main
 struct MemorizeApp: App {
-    @StateObject private var repo = PersonRepository()
-    
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environmentObject(repo)
+            Text("Repository rebuild in progress")
         }
     }
 }
